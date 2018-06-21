c1b9d71fadcbbcbf7a6adcd0d00496571073205b
risa.morimoto <risa.morimoto@rakuten.com>
2018-06-21 04:33:48 +0000

    rakuten api

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -1,6 +1,11 @@
 <?php
+namespace App\Http\Controllers;
 
-  use \App\Item;
+use Illuminate\Http\Request;
+
+use App\Http\Controllers\Controller;
+
+use \App\Item;
 
   class ItemsController extends Controller
   {
