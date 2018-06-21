d306ecef3b3ba08efc2b585cb5758ca98ff3e6a6
risa.morimoto <risa.morimoto@rakuten.com>
2018-06-21 05:15:13 +0000

    want

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -30,7 +30,9 @@
                             </a>
                             <ul class="dropdown-menu">
                                 <li>
-                                    <a href="#">マイページ</a>
+                                <li>
+                                    <a href="{{ route('users.show', Auth::id()) }}">マイページ</a>
+                                </li>
                                 </li>
                                 <li role="separator" class="divider"></li>
                                 <li>
