23e154ed2e35e5d1bffb3e440405410ca850958d
risa.morimoto <risa.morimoto@rakuten.com>
2018-06-22 01:13:46 +0000

    ranking

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -19,6 +19,18 @@
                                 アイテムを追加
                             </a>
                         </li>
+                        
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                                <span class="glyphicon glyphicon-signal" aria-hidden="true"></span>
+                                ランキング
+                                <span class="caret"></span>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a href="{{ route('ranking.want') }}">Wantランキング</a></li>
+                                <li><a href="{{ route('ranking.have') }}">Haveランキング</a></li>
+                            </ul>
+                        </li>
 
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
@@ -30,9 +42,7 @@
                             </a>
                             <ul class="dropdown-menu">
                                 <li>
-                                <li>
                                     <a href="{{ route('users.show', Auth::id()) }}">マイページ</a>
-                                </li>
                                 </li>
                                 <li role="separator" class="divider"></li>
                                 <li>
